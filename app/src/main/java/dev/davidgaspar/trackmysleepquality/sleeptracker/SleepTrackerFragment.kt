898645fad63b47db4eb1e2c68d43a0398228c600
David Gaspar <davidgaspar.dev@gmail.com>
2023-06-30 19:08:12 -0300

    refactor: use ListAdapter to handle rendering performance

--- app/src/main/java/dev/davidgaspar/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/dev/davidgaspar/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -64,7 +64,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner) {
             it?.let {
-                adapter.data= it
+                adapter.submitList(it)
             }
         }
 
